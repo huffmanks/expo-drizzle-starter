239a04952d6209bf39a0f854a10f64edc57972f1
huffmanks <huffmanks2@gmail.com>
2024-08-11 17:32:32 -0400

    drizzle provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,17 @@
-import "@/global.css";
+import '@/global.css';
 
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Theme, ThemeProvider } from "@react-navigation/native";
-import { PortalHost } from "@rn-primitives/portal";
-import { SplashScreen, Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import * as React from "react";
-import { Platform } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Theme, ThemeProvider } from '@react-navigation/native';
+import { PortalHost } from '@rn-primitives/portal';
+import { SplashScreen, Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform } from 'react-native';
 
-import { ThemeToggle } from "@/components/ThemeToggle";
-import { NAV_THEME } from "@/lib/constants";
-import { useColorScheme } from "@/lib/useColorScheme";
+import { DatabaseProvider } from '@/db/provider';
+import { NAV_THEME } from '@/lib/constants';
+import { useColorScheme } from '@/lib/useColorScheme';
+import { ThemeToggle } from '@/components/ThemeToggle';
 
 const LIGHT_THEME: Theme = {
   dark: false,
@@ -24,7 +25,7 @@ const DARK_THEME: Theme = {
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from "expo-router";
+} from 'expo-router';
 
 // Prevent the splash screen from auto-hiding before getting the color scheme.
 SplashScreen.preventAutoHideAsync();
@@ -35,17 +36,17 @@ export default function RootLayout() {
 
   React.useEffect(() => {
     (async () => {
-      const theme = await AsyncStorage.getItem("theme");
-      if (Platform.OS === "web") {
+      const theme = await AsyncStorage.getItem('theme');
+      if (Platform.OS === 'web') {
         // Adds the background color to the html element to prevent white background on overscroll.
-        document.documentElement.classList.add("bg-background");
+        document.documentElement.classList.add('bg-background');
       }
       if (!theme) {
-        AsyncStorage.setItem("theme", colorScheme);
+        AsyncStorage.setItem('theme', colorScheme);
         setIsColorSchemeLoaded(true);
         return;
       }
-      const colorTheme = theme === "dark" ? "dark" : "light";
+      const colorTheme = theme === 'dark' ? 'dark' : 'light';
       if (colorTheme !== colorScheme) {
         setColorScheme(colorTheme);
 
@@ -64,19 +65,19 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-      {/* <DatabaseProvider> */}
-      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            title: "Starter Base",
-            headerRight: () => <ThemeToggle />,
-          }}
-        />
-      </Stack>
-      <PortalHost />
-      {/* </DatabaseProvider> */}
+      <DatabaseProvider>
+        <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: 'Expo Drizzle Starter',
+              headerRight: () => <ThemeToggle />,
+            }}
+          />
+        </Stack>
+        <PortalHost />
+      </DatabaseProvider>
     </ThemeProvider>
   );
 }
